d8441c86a3cab655797ca8606a11fb0b40c65843
JayJ <JayJ@jaehoui-MacBook-Pro.local>
2013-03-15 09:09:41 +0900

    Initial Commit

---
+++ uploadFTP/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  uploadFTP
+//
+//  Created by JayJ on 13. 3. 15..
+//  Copyright (c) 2013년 JayJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ uploadFTP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  uploadFTP
+//
+//  Created by JayJ on 13. 3. 15..
+//  Copyright (c) 2013년 JayJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
